88faf8934c4cb12e2ed387387cff654f557aa5f9
Rustam Karimov <rkkarimov@alfabank.ru>
2020-11-20 16:30:18 +0400

    feat(*): improve design

--- src/utils/date.ts
+++
@@ -1,9 +0,0 @@
-export function formatDate(date: Date) {
-    const dd = date.getDay();
-    const formattedDD: string = `${dd < 10 && 0}${dd}`;
-
-    const mm = date.getMonth() + 1;
-    const yyyy = date.getFullYear();
-
-    return `${formattedDD}.${mm}.${yyyy}`;
-}
