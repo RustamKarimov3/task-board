7ee5bf8cb61dc11335607a1b0288ea7d47ba7b25
Rustam Karimov <rkkarimov@alfabank.ru>
2020-11-20 16:42:49 +0400

    feat(*): optimize app

--- src/redux/store/taskList/reducer.ts
+++ src/redux/store/taskList/reducer.ts
@@ -1,4 +1,4 @@
-import { cloneDeep } from "lodash";
+import cloneDeep from "lodash/cloneDeep";
 import { TASKS } from "../../../constants/tasks";
 
 import { InferValueTypes } from "../../../types/common";
